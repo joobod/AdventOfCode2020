7cd5eeb1e146ef37167a4ee84cc65ca7ce99d6f3
joobod <54845957+joobod@users.noreply.github.com>
2020-12-07 20:06:28 +0100

    day6_1

---
+++ Day 6/Group.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6
+{
+    class Group
+    {
+        HashSet<char> set = new HashSet<char>();
+        List<char> charsToRemove = new List<char>();
+        public void AddLine2(string line)
+        {
+            Console.WriteLine(line);
+            if (set.Count == 0)
+            {
+                foreach (char ch in line)
+                {
+                    set.Add(ch);
+                }
+            }
+            else
+            {
+                foreach (char ch in set)
+                {
+                    if (!line.Contains(ch))
+                    {
+                        charsToRemove.Add(ch);
+                    }
+                }
+                foreach(char ch in charsToRemove)
+                {
+                    set.Remove(ch);
+                }
+                charsToRemove.Clear();
+            }
+        }
+        public void AddLine(string line)
+        {
+            foreach(char ch in line.ToCharArray())
+            {
+                set.Add(ch);
+            }
+        }
+        
+        public char[] getResults()
+        {
+            char[] res = new char[set.Count];
+            int i = 0;
+            foreach(char ch in set)
+            {
+                res[i++] = ch;
+            }
+            return res;
+
+        }
+
+        public int getSum()
+        {
+            return set.Count;
+        }
+
+
+        
+    }
+}
---
+++ Day 6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] allLines = System.IO.File.ReadAllLines(@"C:\Users\Johannes\Documents\0\AdventOfCode\day6input.txt");
+            List<Group> allGroups = new List<Group>();
+
+            Group gp = new Group();
+            for (int i = 0; i < allLines.Length; i++)
+            {
+                gp.AddLine2(allLines[i]);
+                if (i + 1 == allLines.Length || allLines[i + 1] == "")
+                {
+                    allGroups.Add(gp);
+                    gp = new Group();
+                    i++;
+                }
+            }
+
+            int totsum = 0;
+            foreach (Group group in allGroups)
+            {
+                totsum += group.getSum();
+                Console.WriteLine(group.getSum());
+            }
+            Console.WriteLine(totsum);
+
+        }
+    }
+}
